043f5ad1daf4b1e40ad7aa7f3547fac1a456006b
Fr13drich <frederic.saelens@gmail.com>
2023-05-25 12:08:55 +0200

    ec2 name recette

--- createInventory.sh
+++ createInventory.sh
@@ -1,5 +1,6 @@
 echo "[all]" > inventory
 aws ec2 describe-instances \
    --query 'Reservations[*].Instances[*].PublicIpAddress' \
+   --filter "Name=tag:Name,Values='recette'" \
    --output text >> inventory
 cat inventory
